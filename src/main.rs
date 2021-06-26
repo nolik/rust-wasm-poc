f6eba5da74f4eba01d221ec0be0d08259387e7f4
Ihar Novik <i.novik@godeltech.com>
2021-06-27 01:19:31 +0300

    Change title and idea.

--- src/main.rs
+++ src/main.rs
@@ -21,8 +21,7 @@ impl Component for Model {
     fn view(&self) -> Html {
         html! {
             <main>
-                <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-                <h1>{ "Hello World!" }</h1>
+                <h1>{ "Let's cut the link!" }</h1>
                 <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
             </main>
         }
